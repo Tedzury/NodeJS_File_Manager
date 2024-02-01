18287c028ed8672fe19e076153a2c7e18181a5dd
Tedzury <asdrogachev@gmail.com>
2024-02-01 16:53:22 +0200

    fix: fix bug with path parsing. Fix bugs with zip module

--- src/replInputHandler/replInputHandler.js
+++ src/replInputHandler/replInputHandler.js
@@ -1,11 +1,13 @@
 import parseArgs from '../helpers/parseArgs.js';
 import InvalidInputError from '../helpers/invalidInputError.js';
+import normalizeArgs from '../helpers/normalizeArgs.js';
 
 const replInputHandler = (_commandsList) => async (line) => {
   const [command, ...args] = parseArgs(line);
   try {
     if (Object.hasOwn(_commandsList, command)) {
-      await _commandsList[command](args);
+      const normalArgs = normalizeArgs(args.join(' '));
+      await _commandsList[command](normalArgs);
     } else {
       throw new InvalidInputError();
     }
--- src/zipModule/zipModule.js
+++ src/zipModule/zipModule.js
@@ -3,45 +3,59 @@ import { pipeline } from 'stream/promises';
 import { createReadStream, createWriteStream } from 'fs';
 import { resolve } from 'path';
 import { createBrotliCompress, createBrotliDecompress } from 'zlib';
+import { stat } from 'fs/promises';
+import OperationError from '../helpers/operationError.js';
 
 const zipModule = (_appState) => {
-  const compress = (_args) => {
+  const compress = async (_args) => {
     const [srcFileArg, destFileArg] = _args;
     if (srcFileArg && destFileArg) {
-      const srcFile = resolve(_appState.getCurrDir(), srcFileArg);
-      const destFile = resolve(_appState.getCurrDir(), destFileArg);
-      const readable = createReadStream(srcFile);
-      const zipper = createBrotliCompress();
-      const writable = createWriteStream(destFile);
-      readable.on('error', () => console.log('Operation failed'));
-      zipper.on('error', () => console.log('Operation failed'));
-      writable.on('error', () => console.log('Operation failed'));
-      pipeline(
-        readable,
-        zipper,
-        writable
-      );
+      try {
+        const srcFile = resolve(_appState.getCurrDir(), srcFileArg);
+        const destFile = resolve(_appState.getCurrDir(), destFileArg);
+        const doesSrcExists = (await stat(srcFile)).isFile();
+        if (doesSrcExists) {
+          const readable = createReadStream(srcFile);
+          const zipper = createBrotliCompress();
+          const writable = createWriteStream(destFile);
+          await pipeline(
+            readable,
+            zipper,
+            writable
+          );
+        } else {
+          throw new OperationError();
+        }
+      } catch {
+        console.log('Operation failed');
+      }
     } else {
       throw new InvalidInputError();
     }
   }
 
-  const decompress = (_args) => {
+  const decompress = async (_args) => {
     const [srcFileArg, destFileArg] = _args;
     if (srcFileArg && destFileArg) {
-      const srcFile = resolve(_appState.getCurrDir(), srcFileArg);
-      const destFile = resolve(_appState.getCurrDir(), destFileArg);
-      const readable = createReadStream(srcFile);
-      const zipper = createBrotliDecompress();
-      const writable = createWriteStream(destFile);
-      readable.on('error', () => console.log('Operation failed'));
-      zipper.on('error', () => console.log('Operation failed'));
-      writable.on('error', () => console.log('Operation failed'));
-      pipeline(
-        readable,
-        zipper,
-        writable
-      );
+      try {
+        const srcFile = resolve(_appState.getCurrDir(), srcFileArg);
+        const destFile = resolve(_appState.getCurrDir(), destFileArg);
+        const doesSrcExists = (await stat(srcFile)).isFile();
+        if (doesSrcExists) {
+          const readable = createReadStream(srcFile);
+          const zipper = createBrotliDecompress();
+          const writable = createWriteStream(destFile);
+          await pipeline(
+            readable,
+            zipper,
+            writable
+          );
+        } else {
+          throw new OperationError();
+        }
+      } catch {
+        console.log('Operation failed')
+      }
     } else {
       throw new InvalidInputError();
     }
